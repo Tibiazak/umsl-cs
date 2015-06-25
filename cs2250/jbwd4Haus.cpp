ddf04c79dd41ff29c55207fe3584c64e0b9d768a
Josh <josh.e.bearden@gmail.com>
2015-06-24 21:33:18 -0500

    added working (?) shuffle_array()

--- cs2250/jbwd4Haus.cpp
+++ cs2250/jbwd4Haus.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
-#include <iostream>
+//#include <iostream>
 #include <new>
+#include <time.h>
 using namespace std;
 
 void initialize_array(int * arr, int size)
@@ -13,19 +14,55 @@ void initialize_array(int * arr, int size)
 	}
 }
 
-/* 
-int * shuffle_array(int * a, int b)
+
+int * shuffle_array(int * arr, int size)
 {
-	
+	int * itr = arr; //array iterator
+	srand (time(NULL));
+	int * shuf_arr = new int[size];
+	int * shuf_itr = shuf_arr;
+
+	for (int i = size-1; i >= 1; i--)
+	{
+		j = rand() % i+1;
+		cout << j << endl; //debugging
+		
+		//shuffled array gets arr[j] in the arr[i]th position
+		//shuf_arr[i] = arr[j]
+		for(int k = 0; k < j; k++)
+		{
+			itr++;
+		}
+		for(int k = 0; k < i; k++)
+		{
+			shuf_itr++;
+		}
+		*shuf_itr = *itr;
+
+		//shuffled array gets arr[i] in the arr[j]th position
+		//shuf_arr[j] = arr[i]
+		shuf_itr = shuf_arr;
+		itr = arr;
+		for(int k = 0; k < i; k++)
+		{
+			itr++;
+		}
+		for(int k = 0; k < j; k++)
+		{
+			shuf_itr++;
+		}
+		*shuf_itr = *itr;
+	}
+	return shuf_arr;
 }
-*/
+
 void print_array(int * arr, int size)
 {
 	int * itr = arr;
-	cout << "[";
+	cout << "Original array is: [";
 	for(int i = 0; i < size; i++)
 	{
-		cout << *itr;
+		cout << *itr << " ";
 		itr++;
 	}
 	cout << "]" << endl;
@@ -39,7 +76,6 @@ int main()
 	{
 		cout << "Please enter the size of the array: ";
 		cin >> size;
-		cout << endl;
 		if (0 < size && size <= 52)
 			valid = true;
 	}
@@ -50,8 +86,8 @@ int main()
 	//cout << "After initialize_array, before print_array" << endl;
 	print_array(arr, size);
 	//cout << "After print_array" << endl;
-	//int * shuf_arr = shuffle_array(arr, size);
+	int * shuf_arr = shuffle_array(arr, size);
 	delete[] arr;
-	//delete shuf_arr;
+	delete[] shuf_arr;
 	return 0;
 }
