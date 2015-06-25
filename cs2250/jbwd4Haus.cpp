afd1124cfef475978c00a9bb65ae3343bf38d6b5
Josh <josh.e.bearden@gmail.com>
2015-06-24 20:57:31 -0500

    added a few more debug statements

--- cs2250/jbwd4Haus.cpp
+++ cs2250/jbwd4Haus.cpp
@@ -19,10 +19,10 @@ int * shuffle_array(int * a, int b)
 */
 void print_array(int * arr, int size)
 {
-	cout << "[";
+	cout << "[" << endl;
 	for(int i = 0; i < size; i++)
 	{
-		cout << arr[i];
+		cout << arr[i] << endl;
 	}
 	cout << "]" << endl;
 }
