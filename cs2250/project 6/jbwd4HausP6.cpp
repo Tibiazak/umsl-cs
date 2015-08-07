a812f2c60ae07921d0063379b57f6562e910b392
Josh <josh.e.bearden@gmail.com>
2015-08-07 14:22:29 -0500

    added project 6

---
+++ cs2250/project 6/jbwd4HausP6.cpp
@@ -0,0 +1,60 @@
+using namespace std;
+#include <iostream>
+#include <stack>
+#include <string>
+
+
+string reverseString(string str)
+{
+	stack<char> strStack;
+
+	for(int i = 0; i < str.length(); i++)
+	{
+		mystack.emplace(str[i]);
+	}
+
+	string reversed;
+	for(int i = 0; i < str.length(); i++)
+	{
+		reversed[i] = strStack.top();
+		strStack.pop();
+	}
+
+	return reversed;
+}
+
+void selecSort(int * arr, int size)
+{
+	if(size == 1)
+	{
+		return;
+	}
+	int largest = arr[0];
+	int location = 0;
+	for(int i = 1; i < size; i++)
+	{
+		if(arr[i] > largest)
+		{
+			largest = arr[i];
+			location = i; 
+		}
+	}
+	arr[location] = arr[size-1];
+	arr[size-1] = largest;
+	selecSort(arr, size-1);
+}
+
+int main()
+{
+	string str = "Hello";
+	rts = reverseString(str);
+	cout << str << " " << rts << endl;
+
+	int[] arr = {10, 9, 8, 7, 6, 5};
+	selecSort(arr, 6);
+	for(int i = 0; i < 6; i++)
+	{
+		cout << arr[i] << endl;
+	}
+	return 0;
+}
